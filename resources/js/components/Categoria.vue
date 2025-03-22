309035c66e4e75f236b33c5ab80d45ec39c14b76
Felipe Macedo <fmacedo746@gmail.com>
2025-03-22 08:15:56 -0300

    fix: adicionando from a linha de importação

--- resources/js/components/Categoria.vue
+++ resources/js/components/Categoria.vue
@@ -4,7 +4,7 @@ import { inject, ref } from 'vue';
 import { CIcon } from '@coreui/icons-vue';
 import { cilCaretBottom, cilCaretTop, cilPlus, cilTrash, cilPencil } from '@coreui/icons';
 import 'vue-toast-notification/dist/theme-sugar.css';
-import type {deletarCategoriaType, mudarCategoriaPaiType, mudarCategoriaType, mudarExibirModalType, mudarModoModalType} './Categorias.vue';
+import type {deletarCategoriaType, mudarCategoriaPaiType, mudarCategoriaType, mudarExibirModalType, mudarModoModalType} from './Categorias.vue';
 
 const props = defineProps<{
     categoria: ItemCategoriaType;
@@ -20,7 +20,7 @@ const abrirCategoria = () => {
 
 
 
-const deletarCategoria:deletarCategoriaType = inject('deletarCategoria') ?? ((id: number) => {});
+const deletarCategoria:deletarCategoriaType = inject('deletarCategoria') ?? (async (id: number) => {});
 const mudarExibirModal:mudarExibirModalType = inject('mudarExibirModal') ?? (() => {});
 const mudarCategoria:mudarCategoriaType = inject('mudarCategoria') ?? (() => {});
 const mudarModoModal:mudarModoModalType = inject('mudarModoModal') ?? (() => {});
@@ -47,8 +47,8 @@ const exibirModalAtualizar =  () => {
         <div class="flex flex-row justify-between px-2 w-68 borderborder-cyan-300">
             {{ categoria?.nome }}
             <div class="flex flex-row justify-between px-2 w-24  borderborder-red-600">
-                <CIcon class="text-green-600 hover:cursor-pointer" :icon="cilPlus" v-on:click="exibirModalCadastrar(categoria.id)" width="15" />
-                <CIcon class="text-red-600 hover:cursor-pointer" :icon="cilTrash" v-on:click="deletarCategoria(categoria?.id)" width="15" />
+                <CIcon class="text-green-600 hover:cursor-pointer" :icon="cilPlus" v-on:click="exibirModalCadastrar()" width="15" />
+                <CIcon class="text-red-600 hover:cursor-pointer" :icon="cilTrash" v-on:click="deletarCategoria(categoria?.id ?? 0)" width="15" />
                 <CIcon class="text-blue-600 hover:cursor-pointer" :icon="cilPencil" v-on:click="exibirModalAtualizar()" width="15" />
                 <CIcon class="hover:cursor-pointer" :icon="mostrarFilhos ? cilCaretTop : cilCaretBottom"
                     v-on:click="abrirCategoria" width="15" />
