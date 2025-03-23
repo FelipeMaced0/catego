1007545f068dfd82cf4c1cbc3aaa462b583fa6ea
Felipe Macedo <fmacedo746@gmail.com>
2025-03-23 18:01:57 -0300

    feat: removendo suporte para dark mode

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -70,11 +70,11 @@ const submit = async () => {
     
         <div :class="exibir?'':'hidden'" class="w-full p-4 min-w-[300px] max-w-[500px] max-h-full">
             <!-- Modal content -->
-            <div class="bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <div class="bg-white rounded-lg shadow-sm">
                 <!-- Modal header -->
                 <div
-                    class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    class="flex items-center justify-between p-4 md:p-5 border-b rounded-t  border-gray-200">
+                    <h3 class="text-xl font-semibold text-gray-900 ">
                         {{ modo == 'cadastrar' ? 'Cadastrar' : 'Atualizar' }} Categoria {{ (modo == 'cadastrar' && props.categoriaPai != undefined) ? 'em ' + props.categoriaPai.nome : '' }}
                     </h3>
                     <button v-on:click="mudarExibirModal"
@@ -95,7 +95,7 @@ const submit = async () => {
 
                 </div>
                 <!-- Modal footer -->
-                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b ">
                     <button data-modal-hide="default-modal" type="button" v-on:click="submit"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         {{ modo == 'cadastrar' ? 'Cadastrar' : 'Atualizar' }}</button>
