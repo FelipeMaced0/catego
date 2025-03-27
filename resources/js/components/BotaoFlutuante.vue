3ea9a4519c9bdf21dc8839395479d6ef2d55b36e
Felipe Macedo <fmacedo746@gmail.com>
2025-03-26 22:08:39 -0300

    feat: adicionando botão flutuante para a adição de categorias sem pai

---
+++ resources/js/components/BotaoFlutuante.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+ import { cilPlus } from '@coreui/icons';
+ import CIcon from '@coreui/icons-vue';
+ 
+</script>
+<template>
+    <div @click="$emit('click')" class="absolute bottom-10 right-10 size-10 flex flex-col items-center justify-center bg-blue-600 rounded-md">
+        <CIcon class=" hover:cursor-pointer text-white" :icon="cilPlus" width="25" />
+    </div>
+</template>
