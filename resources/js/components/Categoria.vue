289a171675d0200e1fadc4035b8c8752d2de40d8
Felipe Macedo <fmacedo746@gmail.com>
2025-03-26 22:10:00 -0300

    feat: utilizando o componente speed dail e retorando a ui para aproveitar melhor o espaço

--- resources/js/components/Categoria.vue
+++ resources/js/components/Categoria.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { ItemCategoriaType } from '@/types';
 import { inject, ref } from 'vue';
-import { CIcon } from '@coreui/icons-vue';
-import { cilCaretBottom, cilCaretTop, cilPlus, cilTrash, cilPencil } from '@coreui/icons';
 import 'vue-toast-notification/dist/theme-sugar.css';
-import type { deletarCategoriaType, mudarCategoriaPaiType, mudarCategoriaType, mudarExibirModalType, mudarModoModalType } from './Categorias.vue';
+import type { deletarCategoriaType, mudarCategoriaPaiType, mudarCategoriaType, mudarExibirModalType, mudarModoModalType } from '../pages/Home.vue';
+import SpeedDial from 'primevue/speeddial';
 
 const props = defineProps<{
     categoria: ItemCategoriaType;
@@ -26,6 +25,7 @@ const mudarCategoria: mudarCategoriaType = inject('mudarCategoria') ?? (() => { 
 const mudarModoModal: mudarModoModalType = inject('mudarModoModal') ?? (() => { });
 const mudarCategoriaPai: mudarCategoriaPaiType = inject('mudarCategoriaPai') ?? (() => { });
 
+
 const exibirModalCadastrar = () => {
     mudarModoModal('cadastrar');
     mudarCategoria({ pai_id: props.categoria.id, nome: '', descricao: '' });
@@ -40,31 +40,43 @@ const exibirModalAtualizar = () => {
     mudarExibirModal();
 }
 
+const items = ref([
+    {
+        label: 'Cadastrar',
+        icon: 'pi pi-pencil',
+        command: () => {
+            exibirModalCadastrar()
+        }
+    },
+    {
+        label: 'Atualizar',
+        icon: 'pi pi-file-edit',
+        command: () => {
+            exibirModalAtualizar()
+        }
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-trash',
+        command: () => {
+            deletarCategoria(props.categoria?.id ?? 0)
+        }
+    }
+])
 </script>
 
 <template>
-    <li>
-        <div class="flex flex-row justify-between px-2 my-2 w-full borderborder-cyan-300">
-            <div class="w-full min-w-40">{{ categoria?.nome }}</div>
-
-            <div class="flex flex-row flex-grow justify-between px-1">
-                <div class="w-8 h-8 flex justify-center items-center" v-on:click="exibirModalCadastrar()" >
-                    <CIcon class="text-green-600 hover:cursor-pointer" :icon="cilPlus"
-                        width="15" />
-                </div>
-                <div class="w-8 h-8 flex justify-center items-center" v-on:click="deletarCategoria(categoria?.id ?? 0)">
-                <CIcon class="text-red-600 hover:cursor-pointer" :icon="cilTrash"
-                     width="15" />
-                </div>
-                <div class="w-8 h-8 flex justify-center items-center" v-on:click="exibirModalAtualizar()">
-                <CIcon class="text-blue-600 hover:cursor-pointer" :icon="cilPencil" 
-                    width="15" />
-                </div>
+    <li class="w-full">
+        <div class="w-full flex flex-row justify-between px-2 my-2  borderborder-cyan-300">
+            <div class="flex flex-row gap-x-3">
                 <div class="w-8 h-8 flex justify-center items-center" v-on:click="abrirCategoria">
-                <CIcon class="hover:cursor-pointer" :icon="mostrarFilhos ? cilCaretTop : cilCaretBottom"
-                     width="15" />
+                    <i v-if="mostrarFilhos" class="hover:cursor-pointer pi pi-chevron-right"></i>
+                    <i v-else class="hover:cursor-pointer pi pi-chevron-down"></i>
                 </div>
+                <div class="w-full min-w-40 overflow-hidden">{{ categoria?.nome }}</div>
             </div>
+
+            <SpeedDial :model="items" direction="left" :button-props="{ style: { width: '25px', height: '25px', background:'black', border:'black', borderRadius:'15px' } }"/>
 
         </div>
         <ul class="ml-5" :class="mostrarFilhos ? '' : 'hidden'">
