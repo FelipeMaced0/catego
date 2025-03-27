f72b5f799d5debf8220712d305500510a2fe5831
Felipe Macedo <fmacedo746@gmail.com>
2025-03-26 22:10:50 -0300

    feat: mudando o componente de paginação e removendo os estados para a página home

--- resources/js/components/Categorias.vue
+++ resources/js/components/Categorias.vue
@@ -1,167 +1,40 @@
 <script setup lang="ts">
-import { CategoriaItem, ItemCategoriaType } from '@/types';
-import { provide, ref } from 'vue';
-import CIcon from '@coreui/icons-vue';
-import { cilPlus, cilMagnifyingGlass } from '@coreui/icons';
-
-import { useToast } from 'vue-toast-notification';
+import { inject, ref } from 'vue';
 import 'vue-toast-notification/dist/theme-sugar.css';
-import { TailwindPagination } from 'laravel-vue-pagination';
 import Categoria from './Categoria.vue';
-import Modal from './Modal.vue';
-import axios from 'axios';
+import { CategoriaItem, PaginacaoType } from '@/types';
+import Paginator, { PageState } from 'primevue/paginator';
+import type {getResultsType} from '../pages/Home.vue';
 
+const props = defineProps<{ categorias: PaginacaoType, categoriasFiltradas: Array<CategoriaItem> }>();
+const getResults:getResultsType|Function = inject('getResults') ?? (()=>{});
 
-const timer = ref();
-const categorias = ref({ data: [] });
-const categoriasFiltradas = ref({ query: '', result: [] });
-const $toast = useToast();
-
-export type deletarCategoriaType = typeof deletarCategoria;
-export type mudarModoModalType = typeof mudarModoModal;
-export type mudarExibirModalType = typeof mudarExibirModal;
-export type mudarCategoriaType = typeof mudarCategoria;
-export type mudarCategoriaPaiType = typeof mudarCategoriaPai;
-
-const getResults = async (page = 1) => {
-    try {
-        const response = await fetch(`/api/categorias?page=${page}`);
-        categorias.value = await response.json();
-    } catch (e) {
-        console.log('Error');
-    }
-
+const onPage = (state: PageState) => {
+    getResults(state.page + 1, state.rows);
 }
-getResults();
-
-
-const exibirModal = ref(false);
-const modo = ref("cadastrar");
-const categoriaFilho = ref<CategoriaItem>({ nome: '', descricao: '' });
-const categoriaPai = ref<CategoriaItem | undefined>({ nome: '', descricao: '' });
-
-const mudarModoModal = (tipo: "cadastrar" | "atualizar") => {
-    modo.value = tipo;
-}
-
-const mudarExibirModal = () => {
-    exibirModal.value = !exibirModal.value;
-}
-
-const mudarCategoria = (categoria: CategoriaItem) => {
-    categoriaFilho.value = categoria;
-}
-
-const mudarCategoriaPai = (categoria: CategoriaItem | undefined) => {
-    categoriaPai.value = categoria;
-}
-
-
-
-
-const deletarCategoria = async (id: number) => {
-    try {
-
-
-        const response = await axios.delete(`/api/categoria/${id}`);
-        if (response.status >= 200 && response.status < 300) {
-            $toast.success(response.data.message);
-        }
-
-
-        categorias.value.data = removerObjetoPorId(categorias.value.data, id);
-    } catch (e) {
-        $toast.error('Erro ao realizar a requisição!');
-    }
-}
-
-provide('deletarCategoria', deletarCategoria);
-provide('mudarExibirModal', mudarExibirModal);
-provide('mudarCategoria', mudarCategoria);
-provide('mudarModoModal', mudarModoModal);
-provide('mudarCategoriaPai', mudarCategoriaPai);
-provide('getResults', getResults);
-
-function removerObjetoPorId(objeto: any, id: Number) {
-
-    return objeto.map((categoria: CategoriaItem) => {
-
-        if (categoria.id === id) {
-            return null; // Remove o objeto atual
-        }
-
-        if (categoria.sub_categorias && categoria.sub_categorias.length > 0) {
-            // Percorre os filhos e remove o categoria com o ID especificado
-            categoria.sub_categorias = removerObjetoPorId(categoria.sub_categorias, id);
-        }
-
-        return categoria;
-    }).filter(Boolean);
-}
-
-const textSearch = (e) => {
-    clearTimeout(timer.value)
-
-    timer.value = setTimeout(() => {
-        categoriasFiltradas.value.result = [];
-        filtrarCategorias(categorias.value.data);
-    }, 500)
-}
-
-
-const filtrarCategorias = (conjunto: Array<CategoriaItem>) => {
-
-
-    if (categoriasFiltradas.value.query != "") {
-        conjunto.forEach((elemento: CategoriaItem) => {
-            if (elemento.nome.toLowerCase().includes(categoriasFiltradas.value.query.toLowerCase())) {
-                categoriasFiltradas.value.result.push(elemento);
-            }
-
-            if (elemento.sub_categorias && elemento.sub_categorias.length > 0) {
-
-                filtrarCategorias(elemento.sub_categorias);
-            }
-        });
-    } 
-}
-
-const cadastrarCategoriaSemPai = () => {
-    mudarCategoriaPai(undefined);
-    mudarExibirModal();
-}
-
 </script>
 
 <template>
-    <div class="flex flex-col md:flex-row w-full min-h-[300px]">
-        <div
-            class="flex flex-col items-center w-full min-w-[300px] max-w-[500px] border border-gray-500 rounded-sm shadow-lg p-3">
-            <div class="w-full min-w-[300px] min-h-fit flex flex-row justify-around px-2 borderborder-green-600">
-                <div class="flex flex-row w-46 justify-around items-center  border border-gray-500 rounded-sm my-2">
-                    <input type="text" class="h-8 w-38" placeholder="Digite algo..." maxlength="80"
-                        v-model="categoriasFiltradas.query" v-on:keydown="textSearch">
-                    <CIcon class=" hover:cursor-pointer" :icon="cilMagnifyingGlass" width="25" />
-                </div>
-                <div class="flex flex-row w-38 justify-around items-center">
-                    <div class="hidden md:block text-[12px]">Nova Categoria</div>
-                    <CIcon class="text-green-600 hover:cursor-pointer" :icon="cilPlus"
-                        v-on:click="cadastrarCategoriaSemPai" width="25" />
-                </div>
-            </div>
-            <ul class="w-full">
-                <template
-                    v-for="categoria in (categoriasFiltradas.result.length > 0 ? categoriasFiltradas.result : categorias.data)"
-                    :key="categoria.id">
-                    <Categoria :categoria="categoria" />
-                </template>
-            </ul>
-            <div class="mt-2">
 
-                <TailwindPagination :data="categorias" @pagination-change-page="getResults" />
+    <div class="flex flex-col items-center justify-center w-full min-w-[300px] max-w-[500px] p-3">
 
-            </div>
+        <ul class="w-full flex flex-col gap-y-4 max-h-[400px] overflow-scroll">
+            <template
+                v-for="categoria in (props?.categoriasFiltradas?.length > 0 ? props.categoriasFiltradas : props?.categorias?.data)"
+                :key="categoria.id">
+                <Categoria :categoria="categoria" />
+            </template>
+        </ul>
+        <div class="mt-2">
+            <Paginator :template="{
+                '640px': 'PrevPageLink CurrentPageReport NextPageLink',
+                '960px': 'FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink',
+                '1300px': 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink',
+                default: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink JumpToPageDropdown JumpToPageInput'
+            }" :rows="categorias.per_page" v-on:page="onPage" :totalRecords="categorias.total"
+                :rowsPerPageOptions="[5, 10, 15]" ></Paginator>
         </div>
-        <Modal :exibir="exibirModal" :categoria="categoriaFilho" :categoria-pai="categoriaPai" :modo="modo" />
     </div>
+
+
 </template>
